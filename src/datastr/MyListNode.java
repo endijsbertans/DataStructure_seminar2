2ffd0526623c1b4b0d879ffeac9b6c9584a7685e
Endijs Bertāns <s22bertendi@vea.lv>
2024-03-13 11:55:58 +0200

    MyListNode

--- src/datastr/MyListNode.java
+++ src/datastr/MyListNode.java
@@ -1,23 +1,20 @@
 package datastr;
 
-public class MyListNode {
+public class MyListNode<Ttype> {
+//TODO
+	//1. variables - element, next un previous
 	private Ttype element;
 	private MyListNode next = null;
 	private MyListNode previous = null;
+	//2. get and set
 	public Ttype getElement() {
 		return element;
 	}
 	public void setElement(Ttype element) {
-		if(element !=null)
+		if(element != null)
 			this.element = element;
 		else
 			this.element = (Ttype) new Object();
-	}
-	public MyListNode getPrevious() {
-		return previous;
-	}
-	public void setPrevious(MyListNode previous) {
-		this.previous = previous;
 	}
 	public MyListNode getNext() {
 		return next;
@@ -25,11 +22,21 @@ public class MyListNode {
 	public void setNext(MyListNode next) {
 		this.next = next;
 	}
-	public MyListNode(Ttype element) {
-		setElement();
+	public MyListNode getPrevious() {
+		return previous;
 	}
+	public void setPrevious(MyListNode previous) {
+		this.previous = previous;
+	}
+
+	//3. constructors
+	public MyListNode (Ttype element) {
+		setElement(element);
+	}
+	
+	//4. toString
 	public String toString() {
-		return " " + element;
+		return ""+element;
 	}
 	
 }
